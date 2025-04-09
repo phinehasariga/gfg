92ffd5ad2cbb997dc6b8f815963ba5ea3d59c24c
phinehasariga <phinehasariga@gmail.com>
2025-04-09 22:41:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver program
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().quickSort(arr, 0, n - 1);
+            printArray(arr);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high) {
+        // code here
+        if(low<high){
+            int pivotindex = partition(arr,low,high);
+            quickSort(arr,low,pivotindex-1);
+            quickSort(arr,pivotindex+1,high);
+        }
+    }
+
+    static int partition(int arr[], int low, int high) {
+        // your code here
+        int pivot = arr[high];
+        int i = low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<=pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        
+        return i+1;
+    }
+}
