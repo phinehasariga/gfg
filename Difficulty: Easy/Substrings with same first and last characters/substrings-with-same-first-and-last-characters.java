f77b1c1b7b86779c288e2169f580a1ff7f775b5b
phinehasariga <phinehasariga@gmail.com>
2025-05-15 22:58:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+
+            Solution ob = new Solution();
+            int result = ob.countSubstring(s);
+
+            System.out.println(result);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int countSubstring(String s) {
+        // code here
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        int total = 0;
+        for (int count : frequencyMap.values()) {
+            total += count * (count + 1) / 2;
+        }
+        return total;
+    
+    }
+}
