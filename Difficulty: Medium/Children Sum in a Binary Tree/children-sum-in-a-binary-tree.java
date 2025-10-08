65c753ac39abdace07fa675ecb2fd949ac8c47ae
ARIGA PHINEHAS <phinehasariga@gmail.com>
2025-10-08 11:36:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
@@ -1,5 +1,4 @@
-/*Complete the function below
-Node is as follows:
+/*
 class Node{
     int data;
     Node left,right;
@@ -10,33 +9,25 @@ class Node{
         left = right = null;
     }
 }
-
 */
 class Solution {
-    // Function to check whether all nodes of a tree have the value
-    // equal to the sum of their child nodes.
-    public static int isSumProperty(Node root) {
-        // add your code here
-        if(root == null || (root.left == null && root.right == null)){
-            return 1;
-        }
-        int left = 0;
-        int right = 0;
-        if (root.left != null) {
-            left = root.left.data;
+    public boolean isSumProperty(Node root) {
+        //  code here
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
         }
 
-        // If right child is not null, get its data.
-        if (root.right != null) {
-            right = root.right.data;
-        }
-        
-        if(root.data == left + right && (isSumProperty(root.left) != 0) && (isSumProperty(root.right) != 0)) {
-            
-            return 1;
-        }
-        
-        return 0;
+        int leftVal = 0, rightVal = 0;
+
+        if (root.left != null)
+            leftVal = root.left.data;
+
+        if (root.right != null)
+            rightVal = root.right.data;
+
+
+        return (root.data == leftVal + rightVal)
+                && isSumProperty(root.left)
+                && isSumProperty(root.right);
     }
-    
 }
